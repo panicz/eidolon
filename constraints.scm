c6a09ed3da0b1cea9627017a795e77408c360c8a
Panicz Maciej Godek <godek.maciek@gmail.com>
2017-08-25 10:04:20 +0200

    Slight elaboration (sync)

--- constraints.scm
+++ constraints.scm
@@ -1,5 +1,6 @@
 (use-modules (grand scheme))
 
+
 (define (operator? x)
   ...)
 
@@ -10,9 +11,33 @@
 	(or (is factor used-in? first)
 	    (is factor used-in? second)))))
 
-(define (untangle factor #;from formula)
+
+(define (inverse ...)
   ...)
+
+(define (extract factor #;from source #;into target)
+  (if (equal? factor source)
+      target
+      (let (((<.> first second) source))
+	(cond ((and (is factor used-in? first)
+		    (is factor used-in? second))
+	       'oops)
+	      ((is factor used-in? first)
+	       ...)
+	      ((is factor used-in? right)
+	       ...)))))
+
+(define (untangle factor #;from equation)
+  (let ((('= left right) equation))
+    (cond ((and (is factor used-in? left)
+		(is factor used-in? right))
+	   'oops)
+	  ((is factor used-in? left)
+	   (extract factor #;from left #;into right))
+	  ((is factor used-in? right)
+	   (extract factor #;from right #;into left)))))
+
 
 (e.g.
  (untangle 'a #;from '(= (/ a b) (/ c d))) ===> (* b (/ c d))
- (untangle 'b #;from '(= (/ a b) (/ c d))) ===> ...)
+ (untangle 'b #;from '(= (/ a b) (/ c d))) ===> )
