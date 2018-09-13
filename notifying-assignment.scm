c0b10631a89538ae8ef8864f902e77a39bc7513a
Panicz Maciej Godek <godek.maciek@gmail.com>
2018-09-13 21:15:40 +0200

    Significant simplification of "notifying-assignment.scm"

--- notifying-assignment.scm
+++ notifying-assignment.scm
@@ -6,46 +6,18 @@
 
 (define# (observers object) '())
 
-(define-syntax on-change
-  (lambda (stx)
-    (syntax-case stx ()
-      ((on-change (procedure arguments ...) action)
-       (with-syntax (((parameters ...)
-		      (generate-temporaries #'(arguments ...))))
-	 #'(let ((target procedure)
-		 (parameters arguments)
-		 ...)
-	     (let ((event-source `(,target ,parameters ...)))
-	       (set! (observers event-source)
-		     `(,action . ,(observers event-source)))))))
-       
-      ((on-change variable action)
-       (identifier? #'object)
-       #'(let ((location (module-variable (current-module) 'variable)))
-	   (set! (observers location) `(,action . ,(observers location))))))))
+(define-syntax reference ()
+  ((reference (procedure ...))
+   (list procedure ...))
+  ((reference variable)
+   (module-variable (current-module) 'variable)))
 
+(define-syntax (on-change location action)
+  (let ((target (reference location)))
+    (set! (observers target) `(,action . ,(observers target)))))
 
-(define-syntax notifying-set!
-  (lambda (stx)
-    (syntax-case stx ()
-      ((notifying-set! (procedure arguments ...) expression)
-       (with-syntax (((parameters ...)
-		      (generate-temporaries #'(arguments ...))))
-	 #'(let ((value expression)
-		 (target procedure)
-		 (parameters arguments)
-		 ...)
-	     (let ((notifications (observers `(,target ,parameters ...))))
-	       (set! (procedure arguments ...) value)
-	       (for deliver in notifications
-		 (deliver value))))))
-      
-      
-      ((notifying-set! variable expression)
-       (identifier? #'variable)
-       #'(let* ((value expression)
-		(location (module-variable (current-module) 'variable))
-		(notifications (observers location)))
-	   (variable-set! location value)
-	   (for deliver in notifications
-	     (deliver value)))))))
+(define-syntax (notifying-set! target expression)
+  (let ((value expression)
+	(location (reference target)))
+    (for action in (observers location)
+      (action value))))
