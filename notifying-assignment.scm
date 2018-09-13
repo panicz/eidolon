b58d9936f0d4b117ebc3cd36cd8299fe8bc3cd13
Panicz Maciej Godek <godek.maciek@gmail.com>
2018-09-14 00:01:05 +0200

    Preventing notifications from variables that have already been assigned

--- notifying-assignment.scm
+++ notifying-assignment.scm
@@ -2,7 +2,13 @@
   #:use-module (grand scheme)
   #:use-module (assignable-procedures)
   #:replace ((notifying-set! . set!))
-  #:export-syntax (on-change))
+  #:export (observers inhibited? on-inhibited-location-update)
+  #:export-syntax (on-change reference))
+
+(define (assert proposition)
+  (let ((result proposition))
+    (unless result
+      (error "Assertion failed: "'proposition" (got "result")"))))
 
 (define# (observers object) '())
 
@@ -13,11 +19,27 @@
    (module-variable (current-module) 'variable)))
 
 (define-syntax (on-change location action)
+  (assert (procedure? action))
   (let ((target (reference location)))
     (set! (observers target) `(,action . ,(observers target)))))
 
+(define on-inhibited-location-update
+  (make-parameter (lambda (location value)
+		    (warn "Inhibited update of "location" to "value))))
+
+(define inhibited?
+  (make-parameter (lambda# (location) #false)))
+
 (define-syntax (notifying-set! target expression)
-  (let ((value expression)
+  (let ((new-value expression)
+	(old-value target)
 	(location (reference target)))
-    (for action in (observers location)
-      (action value))))
+    (when (isnt new-value equal? old-value)
+      (cond (((inhibited?) location)
+	     ((on-inhibited-location-update) 'target new-value))
+	    (else
+	     (set! target new-value)
+	     (set! ((inhibited?) location) #true)
+	     (for action in (observers location)
+	       (action new-value))
+	     (unset! ((inhibited?) location)))))))
