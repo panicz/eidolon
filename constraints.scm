38680e701e36284703cd96726ab87677ef4b6541
Panicz Maciej Godek <godek.maciek@gmail.com>
2018-09-14 23:51:17 +0200

    The "impose" macro is now expressed with (notifying-assignment) and handles multiple constraints on one variable properly

--- constraints.scm
+++ constraints.scm
@@ -1,12 +1,20 @@
 (define-module (constraints)
   #:use-module (grand scheme)
   #:use-module (assignable-procedures)
-  #:export (keep-updating!)
-  #:replace ((assign! . set!))
-  #:export-syntax (assign! impose))
+  #:use-module (notifying-assignment)
+  #:export-syntax (impose)
+  #:export (constraint-variables untangle))
+
+(use-modules (grand scheme)
+	      (assignable-procedures)
+	       (notifying-assignment))
+
 
 (define (operator? x)
   (is x member '(+ - / *)))
+
+(define (comparator? x)
+  (is x member '(=)))
 
 (define (used-in? factor formula)
   (or (equal? factor formula)
@@ -14,6 +22,12 @@
 		 ((operator? <.>)))
 	(or (is factor used-in? first)
 	    (is factor used-in? second)))))
+
+(e.g.
+ (is 'x used-in? '(* x 2)))
+
+(e.g.
+ (is '(f x) used-in? '(* 2 (f x))))
 
 (define (extract-left operation formula)
   (match operation
@@ -47,8 +61,9 @@
   (if (equal? factor source)
       target
       (let ((`(,<.> ,first ,second) source))
-	(cond ((and (is factor used-in? first)
-		    (is factor used-in? second))
+	(cond ((or (isnt <.> operator?)
+		   (and (is factor used-in? first)
+			(is factor used-in? second)))
 	       'oops)
 	      ((is factor used-in? first)
 	       (let ((target* (extract-left source target)))
@@ -70,110 +85,51 @@
 (e.g.
  (untangle 'a #;from '(= (/ a b) (/ c d))) ===> (* (/ c d) b))
 
-(define (factor? x)
-  (or (number? x)
-      (symbol? x)))
-
 (define (factors formula)
-  (if (factor? formula)
-      `(,formula)
-      (let ((`(,operator . ,operands) formula))
-	(apply union (map factors operands)))))
-
-(define-syntax (assert property)
-  (unless property
-    (error "Assertion failed: " 'property (current-source-location))))
+  (or (and-let* ((`(,<.> . ,operands) formula)
+		 ((or (operator? <.>) (comparator? <.>))))
+	(apply union (map factors operands)))
+      `(,formula)))
 
 (e.g.
- (factors '(= (/ a b) (/ c d))) ===> (c d b a))
+ (is (factors '(= (/ a b) (/ c d)))
+     same-set?
+     '(a b c d)))
 
 (e.g.
- (factors '(= (+ (* x 5) 7) 0)) ===> (0 7 5 x))
+ (is (factors '(= (* (f x) (g y)) (z w)))
+     same-set?
+     '((f x) (g y) (z w))))
 
-(e.g.
- (untangle 'x '(= (+ (* x 5) 7) 0)) ===> (/ (- 0 7) 5))
+(define (constraint-variables formula)
+  (filter (isnt _ number?) (factors formula)))
 
-(define (variables formula)
-  (filter symbol? (factors formula)))
+(define-macro (impose constraint)
+  (let ((expressions (constraint-variables constraint))
+	(_ (make-symbol "_")))
+    `(begin . ,(map (lambda (expression)
+		      (let ((others (delete expression
+					    #;from expressions)))
+			`(on-change ,expression
+				    (lambda ,_
+				      . ,(map (lambda (observer)
+						`(set! ,observer
+						       ,(untangle observer
+								  constraint)))
+					      others)))))
+		    expressions))))
 
-(define# (observers variable) '())
-
-(define# (formula variable) #f)
-
-(define# (dependencies variable) '())
-
-(define (possibly-fresh-variable name)
-  (let ((module (current-module)))
-    (or (module-variable module name)
-	(let ((fresh (make-undefined-variable)))
-	  (module-define! module name fresh)
-	  fresh))))
-
-(define (keep-updating! observer #;about source #;using recipe)
-  ;;(assert (and (symbol? observer?) (symbol? source)))
-  (let* ((observer-variable (possibly-fresh-variable observer))
-	 (source-variable (possibly-fresh-variable source))
-	 (source-observers (observers source-variable))
-	 (old-recipe (formula observer-variable))) 
-    (unless (is observer-variable member source-observers)
-      (set! (observers source-variable) `(,observer-variable
-					  . , source-observers)))
-    (when (and old-recipe (isnt recipe equal? old-recipe))
-      (warn "Overwriting" old-recipe "with" recipe "in" observer))
-    (set! (formula observer-variable) recipe)))
-
-(define-syntax (impose constrain)
-  (for var in (variables 'constrain)
-    (let* ((formula (untangle var #;from 'constrain))
-	   (value-dependencies (variables formula)))
-      (set! (dependencies var) value-dependencies)
-      (for dependency in value-dependencies
-	(keep-updating! var #;about dependency #;using formula)))))
-
-(define (recalculate variable)
-  (when (every defined? (dependencies variable))
-    (eval (formula variable) (current-module))))
-
-(define (update! variables+values updated)
-  (if (null? variables+values)
-      updated
-      (let* ((modified (filter-map (lambda (`(,variable . ,value))
-				     (and (isnt value equal?
-						(variable-ref variable))
-					  (variable-set! variable value)
-					  variable))
-				   variables+values))
-	     (updated (union updated modified))
-	     (notified (difference (fold-left union '() (map observers modified))
-				   updated))
-	     (changes (map (lambda (observer)
-			     `(,observer . ,(recalculate observer)))
-			   notified)))
-	(update! changes updated))))
-
-(define-syntax assign*
-  (syntax-rules ()
-    ((assign* variables+values)
-     (assing* variables+values ()))
-    
-    ((assign* () ((variable value) ...))
-     (update!
-      `((,(module-variable (current-module) 'variable) . ,value) ...) '()))
-    
-    ((assign* (variable value . rest) processed)
-     (assign* rest ((variable value) . processed)))))
-
-(define-syntax (assign! variable value . rest)
-  (assign* (variable value . rest) ()))
+#|
+;; We don't want these symbols to clutter global namespace
 
 (e.g.
  (begin
    (define x 5)
    (define y 1/5)
    (impose (= x (/ 1 y)))
-   (assign! x 6)
+   (set! x 6)
    y) ===> 1/6)
-
+ 
 (e.g.
  (begin
    (define a 2)
@@ -181,5 +137,7 @@
    (define c 6)
    (impose (= (* a b) c))
    (print (observers a))
-   (assign! a 5 b 10)
-   c) ===> 50)
+   (set! a 5)
+   (values a b c))
+ ===> 5 3 15)
+|#
